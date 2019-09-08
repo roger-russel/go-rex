33a2756ebbe517d201c4f8e4a984c18e2d4efcd1
Roger Russel <roger@rrussel.org>
2019-09-08 11:05:20 -0300

    fix unknow error LoL

--- docs/examples/match/main_test.go
+++ docs/examples/match/main_test.go
@@ -4,7 +4,6 @@ func ExampleMain() {
 
 	main()
 	// Output:
-	// /^[a-zA-Z0-9_.+-]+@[a-zA-Z-]+\.[a-z.A-Z0-9.-]+$/ matched fake@gmail.com.br
-	// /^[[:alnum:]_.+-]+@[[:alpha:]]+\.[[:alnum:].-]+$/ matched fake@gmail.com.br
-
+	// /^[a-zA-Z0-9_.+-]+@[a-zA-Z-]+\.[a-z.A-Z0-9.-]+$/ matched fake@email.com.br
+	// /^[[:alnum:]_.+-]+@[[:alpha:]]+\.[[:alnum:].-]+$/ matched fake@email.com.br
 }
